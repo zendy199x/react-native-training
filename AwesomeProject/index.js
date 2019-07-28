aa0c1de7606c7e1f809f731b4f5f528320674dbd
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-29 00:15:31 +0700

    state

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 // import App from './App';
 // import HelloWorld from './components/HelloWorld';
 // import Robot from './components/Robot';
-import MultipleGreetings from './components/MultipleGreetings';
+// import MultipleGreetings from './components/MultipleGreetings';
+import TextBlink from './components/TextBlink';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => MultipleGreetings);
+AppRegistry.registerComponent(appName, () => TextBlink);
