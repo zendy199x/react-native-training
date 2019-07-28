59f2e4c8deaf06d1a2cbb68a9a3a7ea567c73b21
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-29 01:06:16 +0700

    stylesheet

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -2,12 +2,45 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import React, { Component } from 'react';
 // import App from './App';
 // import HelloWorld from './components/HelloWorld';
 // import Robot from './components/Robot';
 // import MultipleGreetings from './components/MultipleGreetings';
-import TextBlink from './components/TextBlink';
+// import TextBlink from './components/TextBlink';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => TextBlink);
+export default class App extends Component {
+    render() {
+        return(
+            <View style={ styles.container }>
+                <Text style = { styles.firstText }>First line</Text>
+                <Text style = { styles.secondText }>Second line</Text>
+                <Text style = { [styles.secondText, styles.firstText] }>Three line</Text>
+                <Text style = { [styles.firstText, styles.secondText] }>Four line</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        backgroundColor: '#28B463',
+        borderWidth: 2,
+        borderColor: '#1F618D'
+    },
+    firstText: {
+        margin: 5, color: 'black'
+    },
+    secondText: {
+        margin: 5,
+        color: 'yellow',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+});
+
+AppRegistry.registerComponent(appName, () => App);
