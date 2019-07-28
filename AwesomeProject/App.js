88210943fd2ce8120d3890861eeacada5043673c
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-28 22:23:59 +0700

    update

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -10,7 +10,7 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'What the fuck .Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
