9d9db65f49c3596ff219f60e9be5627aa4d1aa1f
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-29 02:29:44 +0700

    alignItems

--- AwesomeProject/components/JustifyContentExample.js
+++ AwesomeProject/components/JustifyContentExample.js
@@ -8,15 +8,16 @@ export default class JustifyContentExample extends Component {
                 backgroundColor: 'aquamarine',
                 flex: 1,
                 flexDirection: 'column',
-                // justifyContent: 'flex-start'
+                justifyContent: 'flex-start',
                 // justifyContent: 'flex-end'
                 // justifyContent: 'space-between'
                 // justifyContent: 'space-around'
-                justifyContent: 'center'
+                // justifyContent: 'center'
+                alignItems: 'stretch'
                 } }>
                 <Text style={{width: 50, height: 50, backgroundColor: 'red'}}></Text>
                 <Text style={{width: 50, height: 50, backgroundColor: 'green'}}></Text>
-                <Text style={{width: 50, height: 50, backgroundColor: 'blue'}}></Text>
+                <Text style={{height: 50, backgroundColor: 'blue'}}></Text>
             </View>
         );
     }
