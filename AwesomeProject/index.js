bd19f0c44e8401d70495f52acf6fa8327cfebc80
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-29 01:59:50 +0700

    width, height & flex

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,46 +1,23 @@
 /**
  * @format
  */
-
+import { AppRegistry } from 'react-native';
 import React, { Component } from 'react';
 // import App from './App';
 // import HelloWorld from './components/HelloWorld';
 // import Robot from './components/Robot';
 // import MultipleGreetings from './components/MultipleGreetings';
 // import TextBlink from './components/TextBlink';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+// import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+// import FixedDimension from './components/FixedDimension'
+import FlexDimension from './components/FlexDimension';
 import {name as appName} from './app.json';
 
-export default class App extends Component {
-    render() {
-        return(
-            <View style={ styles.container }>
-                <Text style = { styles.firstText }>First line</Text>
-                <Text style = { styles.secondText }>Second line</Text>
-                <Text style = { [styles.secondText, styles.firstText] }>Three line</Text>
-                <Text style = { [styles.firstText, styles.secondText] }>Four line</Text>
-            </View>
-        )
-    }
-}
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 20,
-        marginLeft: 10,
-        marginRight: 10,
-        backgroundColor: '#28B463',
-        borderWidth: 2,
-        borderColor: '#1F618D'
-    },
-    firstText: {
-        margin: 5, color: 'black'
-    },
-    secondText: {
-        margin: 5,
-        color: 'yellow',
-        fontWeight: 'bold',
-        fontSize: 20
-    }
-});
+// export default class FixedDimension extends Component {
+//     render() {
+//         return(
 
-AppRegistry.registerComponent(appName, () => App);
+//         )
+//     }
+// }
+AppRegistry.registerComponent(appName, () => FlexDimension);
