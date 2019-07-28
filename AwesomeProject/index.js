5d62957588730f0e3575c22179e6a93073e88750
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-28 23:34:22 +0700

    props

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -4,7 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+// import Robot from './components/Robot';
+import MultipleGreetings from './components/MultipleGreetings';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HelloWorld);
+AppRegistry.registerComponent(appName, () => MultipleGreetings);
