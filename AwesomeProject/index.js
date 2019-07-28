5f083fef6853e9f5f89edfe6bcf799f381de6718
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-07-29 04:25:44 +0700

    textInput email &password

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,10 +1,69 @@
 /**
  * @format
  */
-import { AppRegistry } from 'react-native';
 import React, { Component } from 'react';
-// import FlexExample from './components/FlexExample';
-import JustifyContentExample from './components/JustifyContentExample'
+import { AppRegistry, TextInput, View, Text } from 'react-native';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => JustifyContentExample);
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            typedText: 'please type yout text',
+            typedPassword: ''
+        };
+    }
+    render() {
+        return(
+            <View>
+                <TextInput style={ {
+                    height: 40,
+                    margin: 50,
+                    padding: 10,
+                    borderColor: 'gray',
+                    borderWidth: 1
+                } }
+                keyboardType='email-address'
+                placeholder='Enter your email'
+                placeholderTextColor='red'
+                onChangeText={
+                    (text) => {
+                        this.setState(
+                            (previousState) => {
+                                return {
+                                    typedText: text
+                                };
+                            }
+                        );
+                    }
+                }
+                />
+                <TextInput style={ {
+                    height: 40,
+                    margin: 50,
+                    padding: 10,
+                    borderColor: 'gray',
+                    borderWidth: 1
+                } }
+                keyboardType='default'
+                placeholder='Enter your email password'
+                secureTextEntry={ true }
+                onChangeText = {
+                    (text) => {
+                        {this.setState(
+                            () => {
+                                return {
+                                    typedPassword: text
+                                }
+                            }
+                        )}
+                    }
+                }
+                />
+                <Text style={ {marginLeft: 50} }>{ this.state.typedText }</Text>
+            </View>
+        )
+    }
+}
+
+AppRegistry.registerComponent(appName, () => App);
